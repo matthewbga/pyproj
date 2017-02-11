9dcecbd015f8430d423ada1e4d9f90076ec68aef
Matthew Brown <matthew.brown@ehi.com>
2017-02-10 23:24:40 -0500

    Add turn_crank() to set up turn-based game style, along with standard turn display

--- pyproj.py
+++ pyproj.py
@@ -5,25 +5,36 @@
 #imports
 from random import *
 
-EMPTY_POSITION = '.'
+EMPTY_POSITION = '-'
 ROOM_HEIGHT = 5
 ROOM_WIDTH = 5
 
 #classes
 class Game():
-        def __init__(self):
+        def __init__(self, player):
                 self.rooms = []
-                self.characters = []
+                self.characters = [player]
+                self.player = player
+
+        def show(self):
+                room = self.player.room
+                room.show()
+                self.player.show()
+
+        def turn_crank(self):
+                for room in self.rooms:
+                    room.reset_area(self.characters)
+                self.show()
 
 class Hero():
-        def __init__(self, name, room, row, col):
+        def __init__(self, name, row, col):
                 self.level = 1
                 self.health = 100
                 self.inventory = []
                 self.name = name
-                self.char = "t"
+                self.char = "+"
 
-                self.room = room
+                self.room = None
                 self.row = row
                 self.col = col
 
@@ -32,6 +43,12 @@ class Hero():
         def add_item(self,item):
                 self.inventory += item
 
+        def show(self):
+                print("== {}'s Stats ==".format(self.name))
+                print("Level: {}".format(self.level))
+                print("Health: {}".format(self.health))
+                print("Holding: {}".format(", ".join(self.inventory)))
+
 class Room():
         def __init__(self, name):
                 self.name = name
@@ -39,56 +56,50 @@ class Room():
                 self.mobs = []
                 self.entities = {}
 
-                # setup field of play for this room
-                self.area = []
+                # # initialize field of play for this room
+                # self.area = []
 
-        def reset_area(self, game):
+        def reset_area(self, characters):
+                self.area = []
                 for row in range(ROOM_HEIGHT):
                         self.area.append([])
                         for col in range(ROOM_WIDTH):
                                 self.area[row].append(EMPTY_POSITION)
-                for character in game.characters:
+                for character in characters:
                         if character.room == self:
                                 self.area[character.row][character.col] = character.char
 
-        # def place_entity(self, entity):
-        #         entity.row =
-        #         self.entities[entity] = entity.position
-        #         self.reset_area()
-
-        def show(self, g):
-                print("======= {} =======".format(self.name))
-                self.reset_area(g)
+        def show(self):
                 for row in self.area:
                         print("".join(row))
 
-game = Game()
+new_name = input("what is your name Hero?").capitalize()
+player = Hero(new_name, row=0, col=2)
+
+game = Game(player=player)
+
 room_1 = Room("Hall of the Mountain King")
 game.rooms.append(room_1)
 
 room_2 = Room("A maze of twisty little passages, all different")
 game.rooms.append(room_2)
 
-new_name = input("what is your name Hero?").capitalize()
-player = Hero(new_name, room=room_1, row=0, col=2)
+player.room = room_1
 
-game.characters.append(player)
+game.turn_crank()
 
-# room_1.place_entity(player)
-room_1.show(game)
-room_2.show(game)
-print("Hello {}. You are level {}, you have {} health and you have {} in your inventory...welcome".format(player.name,player.level,player.health,player.inventory))
 test_1 = input("would you like to grab the apple and add it to your inventory? yes/no ").lower().strip()
 apple = ["apple"]
 if test_1 == "yes":
         player.add_item(apple)
 else:
         pass
-print(player.inventory)
+game.turn_crank()
+
 test_2 = input("would you like to grab the stick and add it to your inventory? yes/no").lower().strip()
 stick = ["stick"]
 if test_2 == "yes":
         player.add_item(stick)
 else:
         pass
-print(player.inventory)
+game.turn_crank()
